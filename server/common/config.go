248e5f796a6275cfe06de117817fd68d47d56348
alexanghh <alexanghh@gmail.com>
2022-02-10 11:23:15 +0800

    Merge from upstream

--- server/common/config.go
+++ server/common/config.go
@@ -5,15 +5,18 @@ import (
 	"fmt"
 	"github.com/tidwall/gjson"
 	"github.com/tidwall/sjson"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"os/user"
+	"path/filepath"
 	"strings"
 	"sync"
 )
 
 var (
-	Config Configuration
+	Config     Configuration
+	configPath string = filepath.Join(GetCurrentDir(), CONFIG_PATH+"config.json")
 )
 
 type Configuration struct {
@@ -51,6 +54,7 @@ type FormElement struct {
 func init() {
 	Config = NewConfiguration()
 	Config.Load()
+	Config.Save()
 	Config.Initialise()
 }
 
@@ -203,9 +207,16 @@ func (this *Form) Iterator() []FormIterator {
 }
 
 func (this *Configuration) Load() {
-	cFile, err := LoadConfig()
+	file, err := os.OpenFile(configPath, os.O_RDONLY, os.ModePerm)
 	if err != nil {
-		Log.Error("config::load %s", err)
+		Log.Warning("Can't read from config file")
+		return
+	}
+	defer file.Close()
+
+	cFile, err := ioutil.ReadAll(file)
+	if err != nil {
+		Log.Warning("Can't parse config file")
 		return
 	}
 
@@ -273,17 +284,13 @@ func (this *Configuration) Debug() *FormElement {
 }
 
 func (this *Configuration) Initialise() {
-	shouldSave := false
 	if env := os.Getenv("ADMIN_PASSWORD"); env != "" {
-		shouldSave = true
 		this.Get("auth.admin").Set(env)
 	}
 	if env := os.Getenv("APPLICATION_URL"); env != "" {
-		shouldSave = true
 		this.Get("general.host").Set(env).String()
 	}
 	if this.Get("general.secret_key").String() == "" {
-		shouldSave = true
 		key := RandomString(16)
 		this.Get("general.secret_key").Set(key)
 	}
@@ -319,9 +326,6 @@ func (this *Configuration) Initialise() {
 				"label": "Drive",
 			},
 		}
-		shouldSave = true
-	}
-	if shouldSave {
 		this.Save()
 	}
 	InitSecretDerivate(this.Get("general.secret_key").String())
@@ -339,9 +343,14 @@ func (this Configuration) Save() Configuration {
 	})
 	v, _ = sjson.Set(v, "connections", this.Conn)
 
-	if err := SaveConfig(PrettyPrint([]byte(v))); err != nil {
-		Log.Error("config::save %s", err.Error())
+	// deploy the config in our config.json
+	file, err := os.Create(configPath)
+	if err != nil {
+		Log.Error("Filestash needs to be able to create/edit its own configuration which it can't at the moment. Change the permission for filestash to create and edit `%s`", configPath)
+		return this
 	}
+	defer file.Close()
+	file.Write(PrettyPrint([]byte(v)))
 	return this
 }
 
